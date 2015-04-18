c4c5be1db4185d2e001d6b8a71b05d1c273bee0b
George Wilson <georgejameswilson@googlemail.com>
2015-04-18 14:36:28 +0100

    Start work on the install script for #4

---
+++ mod_slideset/script.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @package    JJ_Social_Slider
+ * @author     JoomJunk <admin@joomjunk.co.uk>
+ * @copyright  Copyright (C) 2011 - 2014 JoomJunk. All Rights Reserved
+ * @license    GPL v3.0 or later http://www.gnu.org/licenses/gpl-3.0.html
+ */
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Slideset installation script class.
+ *
+ * @since  1.0
+ */
+class Mod_SlidesetInstallerScript
+{
+	/**
+	 * Function called before module installation/update/removal procedure commences
+	 *
+	 * @param   string                   $type    The type of change (install, update or discover_install
+	 *                                            , not uninstall)
+	 * @param   JInstallerAdapterModule  $parent  The class calling this method
+	 *
+	 * @return  void
+	 *
+	 * @since  1.3.3
+	 */
+	public function preflight($type, $parent)
+	{
+		$jVersion             = new JVersion;
+		$minimumJoomlaVersion = '3.4.0';
+		
+		if (!$version->isCompatible($minimumJoomlaVersion))
+		{
+			echo JText::sprintf('MOD_SLIDESET_JOOMLA_NOT_COMPATIBLE', $minimumJoomlaVersion);
+
+			return false;
+		}
+	}
+
+	/**
+	 * Function called on install of module
+	 *
+	 * @param   JInstallerAdapterModule  $parent  The class calling this method
+	 *
+	 * @return  void
+	 *
+	 * @since  1.3.3
+	 */
+	public function install($parent)
+	{
+		if (JFolder::create(JPATH_ROOT . '/images/slideset'))
+		{
+			echo JText::_('MOD_SLIDESET_COULD_NOT_CREATE_IMAGES_DIR');
+		}
+	}
+}
